315005f316b34867e7ae389cb6244db4ea770832
GitHub Action <action@github.com>
2021-02-02 14:42:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ugurkoysuren/shareyourcode/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ugurkoysuren.shareyourcode.services
+
+import com.github.ugurkoysuren.shareyourcode.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/ugurkoysuren/shareyourcode/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ugurkoysuren/shareyourcode/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ugurkoysuren.shareyourcode.services
 
+import com.github.ugurkoysuren.shareyourcode.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
